3ab34c1f0f1cb657a8e4c24db56acafe1704164f
evilmrmunchkinsman <gigglesglue0>
2026-03-05 13:21:10 +0530

    day 3 i guess?

--- index.js
+++ index.js
@@ -28,6 +28,37 @@ app.post('/todos',(req,res) =>{
 
 app.get('/todos/:id',(req,res) => {
 res.send(req.params)
+const id = Number(req.params.id);
+const todo= todos.find(todo => todo.id ==id);
+if(!todo){
+	 return res.status(404).json({message:"TODO NOT FOUND"});
+}
+	res.json(todo);
+
+})
+
+//delete todo
+app.delete('/todos/:id',(req,res) => {
+const id= Number(req.params.id)
+const todo = todos.find(todo => todo.id===id);
+if(!todo){
+return res.status(404).json({message:"todo not found"})
+}
+todos= todos.filter(todo => todo.id !==id);
+res.json({message:"TODO DELETED"})
+});
+
+//update the todo
+app.put('/todos/:id',(req,res) =>{
+const id= Number(req.params.id)
+const todo= todos.find(todo => todo.id===id );
+if(!todo){
+return res.status(404).json({message:"INVALID TODO "})
+}
+const read= req.body.text;
+todo.text= read;
+res.json(todo)
+
 })
  
 app.listen(port,() => {
